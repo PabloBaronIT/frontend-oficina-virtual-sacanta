54e50e01e501402a878cf34165c5f7457fdb834a
OficinaVirtualBaron <patriciotrabajo@gmail.com>
2022-11-14 18:11:05 -0300

    First Commit

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "bootstrap";
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import "bootstrap/dist/css/bootstrap.css";
 
 createApp(App).use(store).use(router).mount("#app");
