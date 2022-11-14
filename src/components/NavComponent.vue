36adef3ec60d193b9a681caf6aeac2b095e61ab9
OficinaVirtualBaron <patriciotrabajo@gmail.com>
2022-11-14 18:44:56 -0300

    Nav

--- src/components/NavComponent.vue
+++ src/components/NavComponent.vue
@@ -1,11 +1,61 @@
-<template></template>
+<template>
+  <!-- Sidebar -->
+  <div class="w-25 mx-0">
+    <nav id="sidebarMenu" class="d-lg-block sidebar bg-white">
+      <input class="nav-btn" type="button" value="Servicios" />
+      <input class="nav-btn" type="button" value="Comunicaciones" />
+      <input class="nav-btn" type="button" value="Noificaciones" />
+    </nav>
+  </div>
+</template>
 
 <script>
-import { defineComponent } from "@vue/composition-api";
-
-export default defineComponent({
-  setup() {},
-});
+export default {
+  name: "NavComponent",
+  props: {},
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+/* CSS */
+.nav-btn {
+  background-color: #e1ecf4;
+  border-radius: 20px;
+  border: 1px solid #7aa7c7;
+  box-shadow: rgba(255, 255, 255, 0.7) 0 1px 0 0 inset;
+  box-sizing: border-box;
+  color: #39739d;
+  cursor: pointer;
+  display: inline-block;
+  font-family: -apple-system, system-ui, "Segoe UI", "Liberation Sans",
+    sans-serif;
+  font-size: 13px;
+  font-weight: 400;
+  line-height: 1.15385;
+  margin-top: 10px;
+  padding: 8px 0.8em;
+  position: relative;
+  text-align: center;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  vertical-align: baseline;
+  white-space: nowrap;
+}
+
+.nav-btn:hover,
+.nav-btn:focus {
+  background-color: #b3d3ea;
+  color: #2c5777;
+}
+
+.nav-btn:focus {
+  box-shadow: 0 0 0 4px rgba(0, 149, 255, 0.15);
+}
+
+.nav-btn:active {
+  background-color: #a0c7e4;
+  box-shadow: none;
+  color: #2c5777;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="main">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld />
+    <NavComponent />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+
+import NavComponent from "@/components/NavComponent.vue";
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    NavComponent,
   },
 };
 </script>
