3ed63666041c09600505572ecf3d5209f0f7e134
OficinaVirtualBaron <patriciotrabajo@gmail.com>
2022-11-16 17:48:32 -0300

    CSS details

--- src/components/MuniEnLinea/MainContainerComponent.vue
+++ src/components/MuniEnLinea/MainContainerComponent.vue
@@ -1,11 +1,40 @@
 <template>
   <main class="flex-container">
-    <p>asdasdasd</p>
-
-    <div class="carta-servcio">Opcion</div>
-    <div class="carta-servcio">Opcion</div>
-    <div class="carta-servcio">Opcion</div>
-    <div class="carta-servcio">Opcion</div>
+    <h2>Servicios</h2>
+    <div class="card-container">
+      <div class="card carta-servcio">
+        <img src="@/assets/logo.png" alt="" />
+        Opcion
+      </div>
+      <div class="card carta-servcio">
+        <img src="@/assets/logo.png" alt="" />
+        Opcion
+      </div>
+      <div class="card carta-servcio">
+        <img src="@/assets/logo.png" alt="" />
+        Opcion
+      </div>
+      <div class="card carta-servcio">
+        <img src="@/assets/logo.png" alt="" />
+        Opcion
+      </div>
+      <div class="card carta-servcio">
+        <img src="@/assets/logo.png" alt="" />
+        Opcion
+      </div>
+      <div class="card carta-servcio">
+        <img src="@/assets/logo.png" alt="" />
+        Opcion
+      </div>
+      <div class="card carta-servcio">
+        <img src="@/assets/logo.png" alt="" />
+        Opcion
+      </div>
+      <div class="card carta-servcio">
+        <img src="@/assets/logo.png" alt="" />
+        Opcion
+      </div>
+    </div>
   </main>
 </template>
 
@@ -16,10 +45,49 @@ export default {
 </script>
 
 <style scoped>
+h2 {
+  text-align: left;
+}
+
 .flex-container {
-  width: 100%;
+  height: auto;
   background: rgb(235, 235, 235);
   border-top-left-radius: 30px;
   border-bottom-left-radius: 30px;
+  display: flex;
+  padding: 20px;
+  flex-flow: column wrap;
+}
+
+.card-container {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+
+  flex-flow: row wrap;
+}
+
+.card img {
+  border-radius: 50%;
+  object-fit: cover;
+  width: 60px;
+  border: 1px solid #000;
+}
+
+.card {
+  border-radius: 40% 10px;
+  padding: 10px;
+
+  border: none;
+  box-shadow: 5px 5px 12px #444;
+  width: 100px;
+  margin: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.card:hover {
+  box-shadow: 5px 5px 20px #444;
 }
 </style>
--- src/components/MuniEnLinea/NavComponent.vue
+++ src/components/MuniEnLinea/NavComponent.vue
@@ -1,11 +1,29 @@
 <template>
   <!-- Sidebar -->
-  <div class="w-25 mx-0 border nav-container">
-    <nav id="sidebarMenu" class="d-lg-block sidebar bg-white">
-      <img src="@/assets/logo.png" alt="" style="width: 40px" />
+  <div class="w-25 nav-container">
+    <img
+      class="logo"
+      src="https://morteros.gob.ar/sites/default/files/logo_municipio_0.png"
+      alt=""
+    />
+
+    <div class="usuario">
+      <img class="usuario-img" src="@/assets/logo.png" alt="" />
+      <div class="usuario-details">
+        <a href="">Mi cuenta</a>
+        <p>
+          Usuario, <br />
+          Apellido <br />
+          12345678
+        </p>
+      </div>
+    </div>
+
+    <nav id="sidebarMenu" class="btn-container">
       <input class="nav-btn" type="button" value="Servicios" />
       <input class="nav-btn" type="button" value="Comunicaciones" />
       <input class="nav-btn" type="button" value="Noificaciones" />
+      <input class="nav-btn" type="button" value="Representados" />
     </nav>
   </div>
 </template>
@@ -19,34 +37,57 @@ export default {
 
 <style scoped>
 /* CSS */
-.nav-container {
-  height: 100vh;
+
+.usuario {
   display: flex;
+  flex-flow: row wrap;
+  font-size: 0.7em;
+  height: auto;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
 }
 
+.usuario-img {
+  width: 40px;
+  height: 40px;
+}
+
+.usuario-details {
+  display: flex;
+  flex-flow: column wrap;
+  margin-left: 10px;
+}
+
+.nav-container {
+  height: auto;
+  display: flex;
+  flex-flow: column wrap;
+  justify-content: space-evenly;
+  align-items: center;
+}
+
+.logo {
+  width: 150px;
+  margin: 40px 0;
+}
+
+/* CSS */
 .nav-btn {
   background-color: #e1ecf4;
-  border-radius: 20px;
+  border-radius: 3px;
   border: 1px solid #7aa7c7;
   box-shadow: rgba(255, 255, 255, 0.7) 0 1px 0 0 inset;
-  box-sizing: border-box;
   color: #39739d;
   cursor: pointer;
-  display: inline-block;
-  font-family: -apple-system, system-ui, "Segoe UI", "Liberation Sans",
-    sans-serif;
-  font-size: 13px;
-  font-weight: 400;
-  line-height: 1.15385;
-  margin-top: 10px;
-  padding: 8px 0.8em;
-  position: relative;
+  font-size: 0.8em;
+  outline: none;
+  width: 100%;
+  margin-bottom: 10px;
+  border-radius: 15px;
   text-align: center;
+  text-decoration: none;
   user-select: none;
-  -webkit-user-select: none;
-  touch-action: manipulation;
-  vertical-align: baseline;
-  white-space: nowrap;
 }
 
 .nav-btn:hover,
