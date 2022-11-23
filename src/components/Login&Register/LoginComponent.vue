c46481812c05ff2b336dbd48cf2173041ab3d2f2
OficinaVirtualBaron <patriciotrabajo@gmail.com>
2022-11-23 17:01:44 -0300

    creacion

---
+++ src/components/Login&Register/LoginComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="loginc-container">
+    <form>
+      <input type="number" placeholder="CUIT" v-model="this.cuil" />
+
+      <input type="password" placeholder="Contraseña" v-model="this.password" />
+
+      <input type="button" value="Ingresar" @click="login" />
+    </form>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+
+export default {
+  name: "LoginComponent",
+  data() {
+    return {
+      validar: true,
+    };
+  },
+  methods: {
+    login() {
+      if (this.validar) {
+        this.mockLogin();
+        console.log("ok");
+      }
+    },
+    ...mapActions(["mockLogin"]),
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/Login&Register/RegisterComponent.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="registerContainer">
+    <form>
+      <input name="name" v-model="this.name" type="text" placeholder="Nombre" />
+
+      <input v-model="this.lastname" type="text" placeholder="Apellido" />
+
+      <input
+        type="email"
+        name="email"
+        v-model="this.email"
+        placeholder="Email"
+      />
+
+      <input type="password" v-model="this.password" placeholder="Contraseña" />
+
+      <input type="number" v-model="this.cuil" placeholder="CUIT" />
+
+      <input type="button" value="Registrar" @click="registrar" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RegisterComponent",
+  data() {
+    return {
+      name: "",
+      lastname: "",
+      email: "",
+      password: "",
+      cuil: 0,
+    };
+  },
+  methods: {
+    registrar() {
+      let registro = {
+        name: this.name,
+        lastname: this.lastname,
+        email: this.email,
+        password: this.password,
+        cuil: this.cuil,
+      };
+      console.log(registro);
+      //   restdbService
+      //     .postRegistro(registro)
+      //     .then(function (response) {
+      //       console.log(response);
+      //     })
+      //     .catch(function (error) {
+      //       console.log(error);
+      //     });
+    },
+  },
+};
+</script>
+
+<style scoped></style>
