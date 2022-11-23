3a9083102beb2299174f8252fd2fdb2f8b24e986
OficinaVirtualBaron <patriciotrabajo@gmail.com>
2022-11-23 17:03:18 -0300

    vista protegida MuniEnLinea

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import store from "@/store";
 
 const routes = [
   {
@@ -15,6 +16,7 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/MuniEnLineaView.vue"),
+    meta: { requiresAuth: true },
   },
   {
     path: "/gobierno",
@@ -25,11 +27,32 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/GobiernoView.vue"),
   },
+  {
+    path: "/login",
+    name: "login",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/LoginView.vue"),
+  },
 ];
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((route) => route.meta.requiresAuth)) {
+    if (!store.state.loggedIn) {
+      next("/login");
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,21 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
+  state: {
+    loggedIn: false,
+  },
   getters: {},
-  mutations: {},
-  actions: {},
+  mutations: {
+    loggedIn(state) {
+      state.loggedIn = true;
+      console.log("Estado de ingreso: ", state.loggedIn);
+    },
+  },
+  actions: {
+    mockLogin(context) {
+      context.commit("loggedIn");
+      console.log("ok 2");
+    },
+  },
   modules: {},
 });
