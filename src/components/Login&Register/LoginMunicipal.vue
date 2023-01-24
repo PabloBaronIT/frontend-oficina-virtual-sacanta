9bda676dc89133a4699c0435f0b7556cd950da75
OficinaVirtualBaron <patriciotrabajo@gmail.com>
2023-01-24 18:36:09 -0300

    Login Municipal

---
+++ src/components/Login&Register/LoginMunicipal.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="login-container">
+    <form action="">
+      <FormKit
+        type="text"
+        name="Cuil"
+        id="Cuil"
+        label="Cuil"
+        help="Your full name"
+        placeholder="“Jon Doe”"
+      />
+      <FormKit
+        type="text"
+        name="password"
+        id="password"
+        label="Password"
+        help="Your password"
+        placeholder="********"
+      />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.login-container {
+  display: flex;
+  margin: 50px;
+  justify-content: space-around;
+  align-items: center;
+}
+
+form {
+  box-shadow: 0px 0px 10px #333;
+  display: flex;
+  flex-flow: column wrap;
+  padding: 120px 40px;
+  position: relative;
+  z-index: 1;
+  background: #fff;
+}
+</style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,19 +2,21 @@
   <div class="form-container">
     <div>
       <h2>Municipalidad de Campo Bravo</h2>
-
       <LoginComponent />
+      <LoginMunicipal />
     </div>
   </div>
 </template>
 
 <script>
 import LoginComponent from "@/components/Login&Register/LoginComponent.vue";
+import LoginMunicipal from "@/components/Login&Register/LoginMunicipal.vue";
 
 export default {
   name: "LoginView",
   components: {
     LoginComponent,
+    LoginMunicipal,
   },
 };
 </script>
