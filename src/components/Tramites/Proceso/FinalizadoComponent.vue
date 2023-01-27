633b06e8f64bd39349bb07e59f939d265acbd0b0
OficinaVirtualBaron <patriciotrabajo@gmail.com>
2023-01-27 17:14:18 -0300

    Nuevo

---
+++ src/components/Tramites/Proceso/FinalizadoComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="finalizado-container" v-if="this.modal == true">
+    <h2>El tramite ya casi esta finalizado!</h2>
+    <h3>Al realizar tu pago se te enviara un comprobante del tramite.</h3>
+    <PagarComponent />
+  </div>
+</template>
+<script>
+import PagarComponent from "@/components/Tramites/PagarComponents.vue";
+
+export default {
+  name: FinalizadoComponent,
+  components: {
+    PagarComponent,
+  },
+};
+</script>
+
+<style scoped></style>
