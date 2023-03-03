d641e9eca6ec78518123c584d90be7427e1df484
PabloBaronIT <pablob@pablobaron.com.ar>
2023-03-03 14:57:21 -0300

    Modal

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,43 @@
       "
     />
 
+    <div
+      class="modal fade"
+      id="staticBackdrop"
+      data-bs-backdrop="static"
+      data-bs-keyboard="false"
+      tabindex="-1"
+      aria-labelledby="staticBackdropLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="staticBackdropLabel">
+              Modal title
+            </h1>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body">...</div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
+            <button type="button" class="btn btn-primary">Understood</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <router-view />
   </div>
 </template>
@@ -19,7 +56,7 @@ export default {
       role: "",
     };
   },
-  mounted() {
+  created() {
     window.addEventListener("token-localstorage-changed", (event) => {
       this.role = event.detail.storage;
     });
