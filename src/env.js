2453da4b853365b1d489bf7f4222a1e20bd1ab59
pablob <pablob@pablobaron.com.ar>
2023-11-16 17:20:41 -0300

    gh-pages commit

--- src/env.js
+++
@@ -1,7 +0,0 @@
-export const PASSWORD_HEADER = process.env.VUE_APP_PASSWORD_HEADER;
-export const BASE_URL = process.env.VUE_APP_BASEURL;
-export const CLOUDINARY_URL = process.env.VUE_APP_CLOUDINARY_URL;
-export const CLOUDINARY_UPLOAD_PRESET =
-  process.env.VUE_APP_CLOUDINARY_UPLOAD_PRESET;
-export const GOOGLE_CLIENT_ID = process.env.VUE_APP_GOOGLE_CLIENT_ID;
-export const KEY_MAP = process.env.VUE_APP_KEY_MAP;
--- src/services/dbService.js
+++
@@ -1,9 +0,0 @@
-import jwt_decode from "jwt-decode";
-
-export default {
-  //FUINCION PARA DEVOLVER EL PAYLOAD DEL TOKEN
-  getToken(token) {
-    let asd = jwt_decode(token);
-    return asd;
-  },
-};
